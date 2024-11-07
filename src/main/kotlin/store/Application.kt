021234549be867311797acdacb967bd6eb8c7371
Seongdeok Hong <sdhong0609@gmail.com>
2024-11-07 23:00:39 +0900

    fix: ConvenienceStoreController에 생성자

--- src/main/kotlin/store/Application.kt
+++ src/main/kotlin/store/Application.kt
@@ -1,6 +1,14 @@
 package store
 
+import store.model.file.PromotionsFile
+import store.view.InputView
+import store.view.OutputView
+
 fun main() {
-    val convenienceStoreController = ConvenienceStoreController()
+    val convenienceStoreController = ConvenienceStoreController(
+        outputView = OutputView(),
+        inputView = InputView(),
+        promotionsFile = PromotionsFile()
+    )
     convenienceStoreController.run()
 }
--- src/main/kotlin/store/ConvenienceStoreController.kt
+++ src/main/kotlin/store/ConvenienceStoreController.kt
@@ -1,16 +1,21 @@
 package store
 
+import store.model.file.ProductsFile
+import store.model.file.PromotionsFile
+import store.view.InputView
 import store.view.OutputView
 
-class ConvenienceStoreController {
+class ConvenienceStoreController(
+    private val outputView: OutputView,
+    private val inputView: InputView,
+    private val promotionsFile: PromotionsFile
+) {
 
     fun run() {
-        val outputView = OutputView()
         outputView.printGreetings()
         outputView.printProductsInstructions()
 
-        val promotionFile = PromotionsFile()
-        val promotions = promotionFile.mapToPromotions(promotionFile.readFile())
+        val promotions = promotionsFile.mapToPromotions()
 
         val productsFile = ProductsFile(promotions)
         productsFile.updateFile()
