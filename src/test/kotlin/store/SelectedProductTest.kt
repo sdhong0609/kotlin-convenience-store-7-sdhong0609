47449113de099b035b35486cc810573464d33cbe
Seongdeok Hong <sdhong0609@gmail.com>
2024-11-07 16:58:24 +0900

    test(SelectedProductTest): 구매할 상품 테스트

---
+++ src/test/kotlin/store/SelectedProductTest.kt
@@ -0,0 +1,16 @@
+package store
+
+import org.assertj.core.api.Assertions.assertThat
+import org.junit.jupiter.api.Test
+import store.model.SelectedProduct
+
+class SelectedProductTest {
+
+    @Test
+    fun `사용자가 구매할 상품은 이름과 수량을 가진다`() {
+        val selectedProduct = SelectedProduct("콜라", 2)
+
+        assertThat(selectedProduct.name).isEqualTo("콜라")
+        assertThat(selectedProduct.quantity).isEqualTo(2)
+    }
+}
