fced69a2b67cb1f3e2f7734ef17977f7b488dea0
Seongdeok Hong <sdhong0609@gmail.com>
2024-11-07 13:20:39 +0900

    feat: PromotionsFile 클래스

---
+++ src/main/kotlin/store/PromotionsFile.kt
@@ -0,0 +1,19 @@
+package store
+
+import java.io.File
+
+class PromotionsFile {
+
+    fun readFile(): List<String> {
+        return File("src/main/resources/promotions.md").readLines().drop(1)
+    }
+
+    fun mapToPromotions(rawPromotions: List<String>): List<Promotion> {
+        return rawPromotions.map { it.toPromotion() }
+    }
+
+    private fun String.toPromotion(): Promotion {
+        val (name, quantity, price, startDate, endDate) = this.split(',')
+        return Promotion(name, quantity.toInt(), price.toInt(), startDate, endDate)
+    }
+}
